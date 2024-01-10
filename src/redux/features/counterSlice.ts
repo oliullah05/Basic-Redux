fc83c6e39170a05781df96f56f4db6ff29cc4601
oliullah05 <mdolihasan@gmail.com>
2024-01-10 18:29:03 +0600

    Payload And TypeSafe Redux

--- src/redux/features/counterSlice.ts
+++ src/redux/features/counterSlice.ts
@@ -1,19 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from '@reduxjs/toolkit'
 
-const initialState = { count: 0 }
+type TCounterType = {
+    count:number
+}
+
+const initialState:TCounterType = { count: 0 }
 
 const counterSlice = createSlice({
     name: "counter",
     initialState,
     reducers: {
-        increment: (state, action) => {
+        increment: (state) => {
             state.count = state.count + 1
         },
-        decrement: (state, action) => {
+        incrementByValue: (state, action:PayloadAction<number>) => {
+            state.count = state.count + action.payload
+        },
+        decrement: (state) => {
             state.count = state.count - 1
         }
     }
 })
 
-export const {decrement,increment}= counterSlice.actions;
+export const {decrement,increment,incrementByValue}= counterSlice.actions;
 export default counterSlice.reducer
