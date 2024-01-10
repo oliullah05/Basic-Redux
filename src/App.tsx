6addc28d5391dfd3c6d58e8f0439b1a695550ff0
oliullah05 <mdolihasan@gmail.com>
2024-01-10 20:16:54 +0600

    added redux logger middleware

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
 
       <div className="flex justify-center items-center h-screen ">
-      <button onClick={()=>dispatch(incrementByValue({value:5}))} className="px-4 py-4 bg-green-500 rounded-lg mx-5">increment By 5</button>
+      <button onClick={()=>dispatch(incrementByValue(5))} className="px-4 py-4 bg-green-500 rounded-lg mx-5">increment By 5</button>
       <button onClick={()=>dispatch(increment())} className="px-4 py-4 bg-green-500 rounded-lg mx-5">increment</button>
       <h1 className="text-3xl ">{count}</h1>
       <button onClick={()=>dispatch(decrement())} className="px-4 py-4 bg-green-500 rounded-lg mx-5">decrement</button>
